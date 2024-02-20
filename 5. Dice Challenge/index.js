aa538ac5f42dff7139bd421433da685db427ca53
RiteshGadre <riteshgadre9@gmail.com>
2024-02-21 01:17:59 +0530

    logic added

--- 5. Dice Challenge/index.js
+++ 5. Dice Challenge/index.js
@@ -1,3 +1,7 @@
-let randomNumber1= Math.floor(Math.random()*7) + 1;
+var randomNumber1= Math.floor(Math.random()*6) + 1;
+var randomImage= "dice" + randomNumber1 + ".png";
+var randomImageSource= "./images/" + randomImage;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
 
 
