d326715502a0826e6341664b594e67dc8383d82c
RiteshGadre <riteshgadre9@gmail.com>
2024-02-20 17:50:08 +0530

    logic and design of the code is implemented

--- 3. Palindrome Checker/app.js
+++ 3. Palindrome Checker/app.js
@@ -1,25 +1,23 @@
+const input= document.querySelector('#val');
 const btn= document.querySelector('.btn');
+const output= document.querySelector('.output');
 
-btn.addEventListener('click', ()=> {
-    let val= document.querySelector('#output').value;
+btn.addEventListener("click", ()=>{
+    inputValue= input.value;
+    
+
     let start= 0;
-    let end= val.length - 1;
-    console.log(val);
+    let end= inputValue.length - 1;
     let ok= true;
-    while(start< end){
-        if(val[start]!= val[end]){
+    while(start<end){
+        if(inputValue[start]!== inputValue[end]){
             ok= false;
             break;
         }
         start++;
         end--;
     }
-    
-    if(ok){
-        document.querySelector('.output').innerHTML= "Palindrome";
-    }
-    else{
-        document.querySelector('.output').innerHTML= "Not Palindrome";
-    }
 
+    if(ok) output.innerHTML= "Palindrome";
+    else output.innerHTML= "Not Palindrome";
 })
