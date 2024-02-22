a9ae822adb8c4521c842884491425a99a2bd3196
RiteshGadre <riteshgadre9@gmail.com>
2024-02-22 13:59:48 +0530

    keypress feature added into drum kit challenge

--- 6. Drum Kit Challenge/index.js
+++ 6. Drum Kit Challenge/index.js
@@ -1,9 +1,9 @@
 const btns= document.querySelectorAll(".drum").length;
 for (var i = 0; i < btns; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        // alert("I got Clicked");
-        var audio= new Audio('./sounds/tom-1.mp3');
-        audio.play();
+        var btnValue= this.innerHTML;
+        makeSound(btnValue);
+        
         
     
     });
@@ -11,3 +11,49 @@ for (var i = 0; i < btns; i++){
 }
 
 
+document.addEventListener("keypress", function (e) {
+    makeSound(e.key)
+});
+
+function makeSound(btnValue){
+    switch (btnValue) {
+        case 'w':
+            var audio= new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+
+        case 'a':
+            var audio= new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+
+        case 's':
+            var audio= new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+    
+        case 'd':
+            var audio= new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+
+        case 'j':
+            var audio= new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+
+        case 'k':
+            var audio= new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+
+        case 'l':
+            var audio= new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+
+        default:
+            break;
+    }
+
+}
