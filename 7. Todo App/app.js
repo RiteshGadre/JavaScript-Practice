5e19cdea1272ae8baaaeedbe7182e2fba7aee1e3
RiteshGadre <riteshgadre9@gmail.com>
2024-03-02 22:45:22 +0530

    some minor changes made

--- 7. Todo App/app.js
+++ 7. Todo App/app.js
@@ -1,11 +1,26 @@
+// {
+//     desccription: "",
+//     update(),
+//     delete()
+
+// }
+
+const totalTasks= [];
 var input= document.querySelector("input");
 var tasks= document.querySelector(".tasks")
+var button= document.getElementById("clickButton");
 
-document.addEventListener("click", () =>{
+button.addEventListener("click", () =>{
     var inputValue= input.value;
     var newTask= document.createElement("p");
     newTask.innerHTML= inputValue;
     newTask.id= "task";
+    var deleteButton= document.createElement("button");
+    deleteButton.textContent= "Delete";
+    deleteButton.classList.add("delete-button");
+    newTask.appendChild(deleteButton);
+    totalTasks.push(inputValue);
     tasks.appendChild(newTask);
     input.value= "";
+    
 })
