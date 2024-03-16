c26c95b62e1de0acbc78f9ee6f456a0af3f0142e
RiteshGadre <riteshgadre9@gmail.com>
2024-03-16 19:05:21 +0530

    Design part done for now

--- 7. Todo App/app.js
+++ 7. Todo App/app.js
@@ -5,7 +5,8 @@
 
 const totalTasks= [];
 var input= document.querySelector("input");
-var tasks= document.querySelector(".tasks")
+var tasks= document.querySelector(".tasks");
+tasks.classList.add("my-2")
 var button= document.getElementById("clickButton");
 
 button.addEventListener("click", () =>{ 
@@ -18,14 +19,40 @@ button.addEventListener("click", () =>{
     taskDescription.classList.add("taskdescription");
     taskDescription.textContent= inputValue;
 
-    var status= document.createElement("p");
-    status.classList.add("status");
-    status.textContent= "Not Completed"
+    var op= document.createElement("p");
+    op.classList.add("flex");
+
+    var edit= document.createElement("p");
+    edit.classList.add("edit");
+    edit.classList.add("cursor-pointer");
+    edit.textContent= "Edit"
+
+    var del = document.createElement("p");
+    del.classList.add("del");
+    del.classList.add("cursor-pointer");
+    del.textContent= "Delete"
+
+    op.appendChild(edit);
+    op.appendChild(del);  
+    op.classList.add("flex");
+    op.classList.add("gap-3")
+    op.classList.add("justify-between");
 
     newTask.appendChild(taskDescription);
-    newTask.appendChild(status);
-   
+    newTask.appendChild(op);
+     
     totalTasks.push(newTask);
+
+    // Tailwind design class
+    newTask.classList.add("flex");
+    newTask.classList.add("justify-between");
+    newTask.classList.add("bg-[#011627]")
+    newTask.classList.add("px-3");
+    newTask.classList.add("py-1");
+    newTask.classList.add("rounded");
+    newTask.classList.add("my-1");
+
+    
     tasks.appendChild(newTask);
     input.value= "";
     
